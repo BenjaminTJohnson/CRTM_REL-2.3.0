b22a53beed39b86e4bdd6d840921a1ff2b4ff961
Benjamin T. Johnson <benjamin.t.johnson@noaa.gov>
2018-07-27 13:41:52 -0600

    updated python script for tutorial

--- libsrc/test/plot_check_crtm_output.py
+++ libsrc/test/plot_check_crtm_output.py
@@ -3,32 +3,38 @@ import matplotlib.pyplot as plt
 
 f = open('data.txt', 'r')
 
-TB = []
-
+TBa = []
+Ta = []
+T_Ka = []
 i = 0
 for line in f.readlines():
     #print(repr(line))
-    #'   Brightness Temperature        :  2.577742E+02\n'
+    #'92    1   22    273.3560    242.6359  0.6129E-28\n'
     line = line.strip()
-    #'   Brightness Temperature        :  2.577742E+02'
+    #'92    1   22    273.3560    242.6359  0.6129E-28'
     columns = line.split()
-    #   ['Brightness' 'Temperature' ':'  '2.577742E+02']
-    name = columns[0]+columns[1]+columns[2] #modify this to match your data output columns
-    
-    TB.append(float(columns[3])) #this is a number column, append the list for each item.
-    print(name, TB[i])    
+    #[92    1   22    273.3560    242.6359  0.6129E-28]
+
+    Ta.append(float(columns[3])) #temperature
+    TBa.append(float(columns[4])) #TB
+    T_Ka.append(float(columns[5])) #T jacobian
     i = i + 1
-    
 f.close()
 
-# do some plotting, here's an example.  Modify to fit your data:
-t = np.arange(0.0, 2.0, 0.01)
-s = 1 + np.sin(2*np.pi*t)
-plt.plot(t, s)
+T = np.reshape(Ta,(22,92))
+TB = np.reshape(TBa,(22,92))
+T_K = np.reshape(T_Ka,(22,92))
 
-plt.xlabel('time (s)')
-plt.ylabel('voltage (mV)')
-plt.title('About as simple as it gets, folks')
+# do some plotting, here's an example.  Modify to fit your data:
+print T_K
+channel_number = 5
+
+#example vertical plot of jacobian
+plt.semilogx(T_K[channel_number,0:92],list(reversed(range(92))))
+
+plt.xlabel('Jacobian [TB/K]')
+plt.ylabel('Vertical Levels')
+plt.title('title')
 plt.grid(True)
 plt.savefig("test.png")  #macos command line "open test.png" to view.  Linux, try "display test.png" 
 
